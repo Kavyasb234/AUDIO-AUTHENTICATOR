be41cb534fcbcce88853cdce7d48a22ca7bf4903
Kavyasb234 <kavyasb900@gmail.com>
2026-03-16 22:21:10 +0530

    Update verifier.py

--- verifier.py
+++ verifier.py
@@ -1,11 +1,11 @@
 import numpy as np
 from scipy.io import wavfile
 
-input_file = "signed.wav"
+file = "signed.wav"
 
-sr, data = wavfile.read(input_file)
+sr, data = wavfile.read(file)
 
-# convert stereo to mono
+# convert to mono
 if len(data.shape) > 1:
     data = data.mean(axis=1)
 
@@ -16,8 +16,9 @@ freqs = np.fft.rfftfreq(len(data), 1/sr)
 
 mask = (freqs > 18500) & (freqs < 19000)
 
-if np.max(fft[mask]) > 100000:
+peak = np.max(fft[mask])
+
+if peak > 1000:
     print("ORIGINAL AUDIO VERIFIED")
 else:
     print("EDITED / TAMPERED AUDIO")
-
